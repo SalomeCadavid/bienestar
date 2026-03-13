bae2e16998018fa78e49b40caa10fa9eb7a1349f
Fory <johansfory@gmail.com>
2026-03-13 15:37:58 -0500

    fix auth provider usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,13 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 
 class Usuario extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
     protected $table = 'usuarios';
 
@@ -17,38 +16,20 @@ class Usuario extends Authenticatable
         'nombre',
         'email',
         'password',
+        'rol_id',
+        'genero',
         'edad',
         'peso',
         'estatura',
-        'genero',
-        'imc',
-        'rol_id'
+        'imc'
     ];
 
     protected $hidden = [
-        'password',
-        'remember_token'
-    ];
-
-    protected $casts = [
-        'edad' => 'integer',
-        'peso' => 'decimal:2',
-        'estatura' => 'decimal:2',
-        'imc' => 'decimal:2'
+        'password'
     ];
 
     public function role()
     {
-        return $this->belongsTo(Roles::class, 'rol_id');
-    }
-
-    public function facturas()
-    {
-        return $this->hasMany(Factura::class);
-    }
-
-    public function recomendaciones()
-    {
-        return $this->hasMany(Recomendaciones_Semanales::class, 'usuario_id');
+        return $this->belongsTo(Role::class,'rol_id');
     }
 }
