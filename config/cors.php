e4509f68173f1a8c8d54b237bf982a8d219229c9
Fory <johansfory@gmail.com>
2026-03-26 23:04:16 -0500

    fix login

--- config/cors.php
+++ config/cors.php
@@ -11,7 +11,8 @@ return [
         'http://localhost:5174',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:5174',
-        'https://bienestar-total.vercel.app'
+        'https://bienestar-total.vercel.app',
+        'https://bienestar-frontend-rnxw.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
